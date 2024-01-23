fc7da05de164c079295328200a73d66b409463f1
Aliaksei Rudak <arudak@ebsco.com>
2024-01-23 13:52:22 +0600

    feat: add create fn

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+
+const filePath = "src/fs/files/fresh.txt"
+const message = "I am fresh and young"
+const error = new Error("FS operation failed")
+const { access, writeFile, F_OK } = fs
+
+const checkAndWrite = (e) => {
+    if (!e) throw error
+    writeFile(filePath, message, () => {})
+}
+
 const create = async () => {
-    // Write your code here 
+    access(filePath, F_OK, checkAndWrite)
 };
 
 await create();
