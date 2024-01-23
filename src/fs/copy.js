196e0128c0e5e4123a8a246dc30d16e0e09e2191
Aliaksei Rudak <arudak@ebsco.com>
2024-01-23 19:01:35 +0600

    feat: add copy fn

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,34 @@
+import { readdir, mkdir, copyFile } from "fs/promises"
+import { join } from "path"
+
+const pathFrom = "src/fs/files"
+const pathTo = "src/fs/files_copy"
+const errorMessage = "FS operation failed"
+
+const errorHandler = (e) => {
+    if (e) throw new Error(errorMessage)
+}
+
+const createNewDir = async () => {
+    try {
+        await mkdir(pathTo)
+    } catch (e) {
+        errorHandler(e)
+    }
+    
+}
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const filesList = await readdir(pathFrom)
+        await createNewDir()
+
+        const promiseArr = filesList.map((fileName) => copyFile(join(pathFrom, fileName), join(pathTo, fileName)))
+        await Promise.all(promiseArr);
+    } catch (e) {
+        errorHandler(e)
+    }
+    
 };
 
 await copy();
