839468d46a5bfe3c961e1612d5bc4137569c3153
Aliaksei Rudak <arudak@ebsco.com>
2024-01-23 18:02:16 +0600

    refactor: change fs import

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,9 +1,8 @@
-import fs from "fs";
+import { access, writeFile, F_OK } from "fs";
 
 const filePath = "src/fs/files/fresh.txt"
 const message = "I am fresh and young"
 const error = new Error("FS operation failed")
-const { access, writeFile, F_OK } = fs
 
 const checkAndWrite = (e) => {
     if (!e) throw error
