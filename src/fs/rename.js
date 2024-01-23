f05d926fd2bfa7f3152dd2a2ff289c979ed17bce
Aliaksei Rudak <arudak@ebsco.com>
2024-01-23 20:16:33 +0600

    feat: add rename fn

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import { rename as nodeRename, access } from "fs/promises"
+import { join, dirname } from "path"
+import { fileURLToPath } from "url"
+
+const currentModuleUrl = import.meta.url
+const __dirname = dirname(fileURLToPath(currentModuleUrl))
+
+const wrongFilePath = join(__dirname, "files", "wrongFilename.txt")
+const properFilePath = join(__dirname, "files", "properFilename.md")
+const errorMessage = "FS operation failed"
+
+const checkFileExist = () => new Promise(async(res, rej) => {
+    try {
+        await access(properFilePath)
+        rej(new Error(errorMessage))
+    } catch {
+        res()
+    }
+})
+
 const rename = async () => {
-    // Write your code here 
-};
+    try {
+        await checkFileExist()
+        await nodeRename(wrongFilePath, properFilePath)
+    } catch {
+        throw new Error(errorMessage)
+    }
+}
 
 await rename();
