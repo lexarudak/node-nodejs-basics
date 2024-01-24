8c8370713fa606cb3ab4093c08a29ec627b2742d
Aliaksei Rudak <arudak@ebsco.com>
2024-01-24 18:37:49 +0600

    feat: add write fn

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from "fs"
+import { join, dirname } from "path"
+import { fileURLToPath } from "url"
+import { pipeline } from "stream/promises"
+
+const currentModuleUrl = import.meta.url
+const __dirname = dirname(fileURLToPath(currentModuleUrl))
+const filePath = join(__dirname, "files", "fileToWrite.txt")
+
 const write = async () => {
-    // Write your code here 
+    console.log("- Press ENTER to write row\n- Press Ctrl+C to complete\n\n*Enter your text* ");
+    pipeline(process.stdin, createWriteStream(filePath))
 };
 
 await write();
