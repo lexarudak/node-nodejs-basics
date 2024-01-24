7f823592d8f6d04f511b863dfdf38f7f2f1633a1
Aliaksei Rudak <arudak@ebsco.com>
2024-01-24 18:26:00 +0600

    feat: add read fn

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from "fs"
+import { join, dirname } from "path"
+import { fileURLToPath } from "url"
+import { pipeline } from "stream/promises"
+
+const currentModuleUrl = import.meta.url
+const __dirname = dirname(fileURLToPath(currentModuleUrl))
+const filePath = join(__dirname, "files", "fileToRead.txt")
+
 const read = async () => {
-    // Write your code here 
+    pipeline(createReadStream(filePath), process.stdout)
 };
 
 await read();
