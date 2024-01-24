5ef1f08dc3b66cd7e8d7e3de6088864ccdfe91b9
Aliaksei Rudak <arudak@ebsco.com>
2024-01-24 12:30:53 +0600

    feat: add parseArgs fn

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2)
+
+    const isEven = (ind) => ind % 2 === 0 
+
+    const even = []
+    const notEven = []
+
+    args.forEach((val, ind) => {
+        isEven(ind)? even.push(val.slice(2)) : notEven.push(val)
+    })
+
+    const ans = even.map((val, ind) => `${val} is ${notEven[ind]}`)
+    console.log(ans.join(', '));
 };
 
 parseArgs();
