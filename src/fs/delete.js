d1b91d171f97b932e829b2fc49d723cdb0777200
Aliaksei Rudak <arudak@ebsco.com>
2024-01-24 10:31:21 +0600

    feat: add delete fn

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { unlink } from "fs/promises"
+import { join, dirname } from "path"
+import { fileURLToPath } from "url"
+
+const currentModuleUrl = import.meta.url
+const __dirname = dirname(fileURLToPath(currentModuleUrl))
+
+const filePath = join(__dirname, "files", "fileToRemove.txt")
+const errorMessage = "FS operation failed"
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await unlink(filePath)
+    } catch {
+        throw new Error(errorMessage)
+    }
 };
 
 await remove();
