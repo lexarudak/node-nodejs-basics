b1e428f7a83febd54e64e88f758226528cb0d680
Aliaksei Rudak <arudak@ebsco.com>
2024-01-24 19:14:52 +0600

    feat: add compress fn

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import { createReadStream, createWriteStream } from "fs"
+import { join, dirname } from "path"
+import { fileURLToPath } from "url"
+import { createHash } from "crypto";
+import { pipeline } from "stream/promises"
+
+import { createGzip } from 'zlib';
+
+const currentModuleUrl = import.meta.url
+const __dirname = dirname(fileURLToPath(currentModuleUrl))
+const filePath = join(__dirname, "files", "fileToCompress.txt")
+const archPath = join(__dirname, "files", "archive.gz")
+
 const compress = async () => {
-    // Write your code here 
+    const readStream = createReadStream(filePath)
+    const writeStream = createWriteStream(archPath)
+    const gzip = createGzip()
+
+    await pipeline(
+        readStream,
+        gzip,
+        writeStream
+    )
 };
 
 await compress();
