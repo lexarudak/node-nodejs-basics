0deae14decc6d43b8bd3801cffcfea11cdd53019
Aliaksei Rudak <arudak@ebsco.com>
2024-01-24 10:41:30 +0600

    feat: add read fn

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { readFile } from "fs/promises"
+import { join, dirname } from "path"
+import { fileURLToPath } from "url"
+
+const currentModuleUrl = import.meta.url
+const __dirname = dirname(fileURLToPath(currentModuleUrl))
+
+const filePath = join(__dirname, "files", "fileToRead.txt")
+const errorMessage = "FS operation failed"
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const buffer = await readFile(filePath)
+        console.log(buffer.toString());
+    } catch {
+        throw new Error(errorMessage)
+    }
 };
 
 await read();
